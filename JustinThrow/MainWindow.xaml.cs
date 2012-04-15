4d60c398c10c303d8f65c6b2679c949b791c8f20
Kenneth Pinpin <kenneth.pinpin@gmail.com>
2012-04-15 20:00:36 +0800

    added Kinect toolbox to allow gesture recognition

--- JustinThrow/MainWindow.xaml.cs
+++ JustinThrow/MainWindow.xaml.cs
@@ -7,10 +7,14 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Windows.Media.Animation;
+
+
+
+
+using Microsoft.Research.Kinect.Nui;
+using Kinect.Toolbox;
+using Kinect.Toolbox.Record;
+using System.IO;
 
 namespace JustinThrow
 {
@@ -19,6 +23,21 @@ namespace JustinThrow
     /// </summary>
     public partial class MainWindow : Window
     {
+        Runtime kinectRuntime;
+        readonly SwipeGestureDetector swipeGestureRecognizer = new SwipeGestureDetector();
+        TemplatedGestureDetector circleGestureRecognizer;
+        readonly ColorStreamManager streamManager = new ColorStreamManager();
+        SkeletonDisplayManager skeletonDisplayManager;        
+        readonly BarycenterHelper barycenterHelper = new BarycenterHelper();
+        readonly AlgorithmicPostureDetector algorithmicPostureRecognizer = new AlgorithmicPostureDetector();
+        TemplatedPostureDetector templatePostureDetector;
+        bool recordNextFrameForPosture;
+
+        string circleKBPath;
+        string letterT_KBPath;
+
+        BindableNUICamera nuiCamera;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -51,6 +70,201 @@ namespace JustinThrow
             }
 
 
+        }
+
+        void Kinects_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case KinectStatus.Connected:
+                    if (kinectRuntime == null)
+                    {
+                        kinectRuntime = e.KinectRuntime;
+                        Initialize();
+                    }
+                    break;
+                case KinectStatus.Disconnected:
+                    if (kinectRuntime == e.KinectRuntime)
+                    {
+                        Clean();
+                        MessageBox.Show("Kinect was disconnected");
+                    }
+                    break;
+                case KinectStatus.NotReady:
+                    break;
+                case KinectStatus.NotPowered:
+                    if (kinectRuntime == e.KinectRuntime)
+                    {
+                        Clean();
+                        MessageBox.Show("Kinect is no more powered");
+                    }
+                    break;
+                default:
+                    MessageBox.Show("Unhandled Status: " + e.Status);
+                    break;
+            }
+        }
+
+        private void mainWin_Loaded(object sender, RoutedEventArgs e)
+        {
+            circleKBPath = Path.Combine(Environment.CurrentDirectory, @"data\circleKB.save");
+            letterT_KBPath = Path.Combine(Environment.CurrentDirectory, @"data\t_KB.save");
+
+            try
+            {
+                //listen to any status change for Kinects
+                Runtime.Kinects.StatusChanged += Kinects_StatusChanged;
+
+                //loop through all the Kinects attached to this PC, and start the first that is connected without an error.
+                foreach (Runtime kinect in Runtime.Kinects)
+                {
+                    if (kinect.Status == KinectStatus.Connected)
+                    {
+                        kinectRuntime = kinect;
+                        break;
+                    }
+                }
+
+                if (Runtime.Kinects.Count == 0)
+                    MessageBox.Show("No Kinect found");
+                else
+                    Initialize();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Initialize()
+        {
+            if (kinectRuntime == null)
+                return;
+
+            kinectRuntime.Initialize(RuntimeOptions.UseSkeletalTracking | RuntimeOptions.UseColor);
+            kinectRuntime.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
+            kinectRuntime.SkeletonFrameReady += kinectRuntime_SkeletonFrameReady;
+            kinectRuntime.VideoFrameReady += kinectRuntime_VideoFrameReady;
+
+            swipeGestureRecognizer.OnGestureDetected += OnGestureDetected;
+
+            //skeletonDisplayManager = new SkeletonDisplayManager(kinectRuntime.SkeletonEngine, kinectCanvas);
+
+            kinectRuntime.SkeletonEngine.TransformSmooth = true;
+            var parameters = new TransformSmoothParameters
+            {
+                Smoothing = 1.0f,
+                Correction = 0.1f,
+                Prediction = 0.1f,
+                JitterRadius = 0.05f,
+                MaxDeviationRadius = 0.05f
+            };
+            kinectRuntime.SkeletonEngine.SmoothParameters = parameters;
+
+            LoadCircleGestureDetector();
+            LoadLetterTPostureDetector();
+
+            nuiCamera = new BindableNUICamera(kinectRuntime.NuiCamera);
+
+            //elevationSlider.DataContext = nuiCamera;
+
+            //voiceCommander = new VoiceCommander("record", "stop");
+            //voiceCommander.OrderDetected += voiceCommander_OrderDetected;
+
+            //StartVoiceCommander();
+        }
+
+        void kinectRuntime_VideoFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+            //kinectDisplay.Source = streamManager.Update(e);
+        }
+
+        void kinectRuntime_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            if (e.SkeletonFrame == null)
+                return;
+
+            //if (recorder != null)
+            //    recorder.Record(e.SkeletonFrame);
+
+            if (e.SkeletonFrame.Skeletons.Where(s => s.TrackingState != SkeletonTrackingState.NotTracked).Count() == 0)
+                return;
+
+            ProcessFrame(e.SkeletonFrame);
+        }
+
+        void ProcessFrame(ReplaySkeletonFrame frame)
+        {
+            Dictionary<int, string> stabilities = new Dictionary<int, string>();
+            foreach (var skeleton in frame.Skeletons)
+            {
+                if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
+                    continue;
+
+                barycenterHelper.Add(skeleton.Position.ToVector3(), skeleton.TrackingID);
+
+                stabilities.Add(skeleton.TrackingID, barycenterHelper.IsStable(skeleton.TrackingID) ? "Stable" : "Unstable");
+                if (!barycenterHelper.IsStable(skeleton.TrackingID))
+                    continue;
+
+                if (recordNextFrameForPosture)
+                {
+                    recordNextFrameForPosture = false;
+                    templatePostureDetector.AddTemplate(skeleton);
+                }
+
+                foreach (Joint joint in skeleton.Joints)
+                {
+                    if (joint.Position.W < 0.8f || joint.TrackingState != JointTrackingState.Tracked)
+                        continue;
+
+                    if (joint.ID == JointID.HandRight)
+                    {
+                        swipeGestureRecognizer.Add(joint.Position, kinectRuntime.SkeletonEngine);
+                        circleGestureRecognizer.Add(joint.Position, kinectRuntime.SkeletonEngine);
+                    }
+                }
+
+                algorithmicPostureRecognizer.TrackPostures(skeleton);
+                templatePostureDetector.TrackPostures(skeleton);
+            }
+
+            //skeletonDisplayManager.Draw(frame);
+
+            //stabilitiesList.ItemsSource = stabilities;
+
+            //currentPosture.Text = "Current posture: " + algorithmicPostureRecognizer.CurrentPosture;
+        }
+
+        private void Clean()
+        {
+            swipeGestureRecognizer.OnGestureDetected -= OnGestureDetected;
+
+            CloseGestureDetector();
+
+            ClosePostureDetector();
+
+            //if (voiceCommander != null)
+            //{
+            //    voiceCommander.OrderDetected -= voiceCommander_OrderDetected;
+            //    voiceCommander.Dispose();
+            //    voiceCommander = null;
+            //}
+
+            //if (recorder != null)
+            //{
+            //    recorder.Stop();
+            //    recorder = null;
+            //}
+
+            if (kinectRuntime != null)
+            {
+                kinectRuntime.SkeletonFrameReady -= kinectRuntime_SkeletonFrameReady;
+                kinectRuntime.VideoFrameReady -= kinectRuntime_VideoFrameReady;
+                kinectRuntime.Uninitialize();
+                kinectRuntime = null;
+            }
         }
     }
 }
